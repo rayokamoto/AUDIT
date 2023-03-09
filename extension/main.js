b6a03db01ef1b5efd1b8a50504710d557fd0b3fb
Ray <22254748+rayokamoto@users.noreply.github.com>
2023-03-10 00:39:11 +1030

    style: Format code

--- extension/main.js
+++ extension/main.js
@@ -1,2 +1,29 @@
-"use strict";
-console.log("Hello, World!");
+import "firefox-webext-browser";
+const targetURL = "https://api.adelaide.edu.au/api/generic-query-structured/v1/?target=/system/TIMETABLE_WEEKLY/queryx/*";
+let semCode = 4310; // for testing only
+let studentID = 1886739; // for testing only
+let counter = 0;
+function getTimetable(e) {
+    counter++;
+    let rawData = e.requestHeaders;
+    let token = "";
+    for (const data of rawData) {
+        if (data.name === "Authorization") {
+            token = data.value;
+        }
+    }
+    let timetable = window.fetch(`https://api.adelaide.edu.au/api/generic-query-structured/v1/?target=/system/TIMETABLE_LIST/queryx/${studentID},${semCode}&MaxRows=9999`, {
+        headers: {
+            "access-control-allow-credentials": "true",
+            "access-control-allow-headers": "accept,authorization,access-control-allow-headers,access-control-allow-origin",
+            "access-control-allow-methods": "GET,OPTIONS",
+            "Authorization": token
+        }
+    });
+    console.log(timetable);
+    if (counter === 1) {
+        browser.webRequest.onBeforeSendHeaders.removeListener(getTimetable);
+    }
+    return { requestHeaders: e.requestHeaders, timetable };
+}
+browser.webRequest.onBeforeSendHeaders.addListener(getTimetable, { urls: [targetURL] }, ["blocking", "requestHeaders"]);
