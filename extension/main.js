8587297756ae9738ec86f2c4f78f5ad7fc8ca452
Rishi Adhikari <rishiad@users.noreply.github.com>
2023-03-17 11:11:02 +1030

    updated UI to test parser

--- extension/main.js
+++ extension/main.js
@@ -7,74 +7,14 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-<<<<<<< HEAD
-const targetURL = "https://api.adelaide.edu.au/api/generic-query-structured/v1/?target=/system/TIMETABLE_WIDGET/queryx/*";
-function getIDandCode(url, token) {
-=======
-import "@types/firefox-webext-browser";
-const targetURL = "https://api.adelaide.edu.au/api/generic-query-structured/v1/?target=/system/TIMETABLE_WEEKLY/queryx/*";
-let semCode = 4310;
-let studentID = 1886739;
-function getIDandCode(e) {
-    let rawData = e;
-    console.log(rawData);
-}
-function getTimetable(e) {
->>>>>>> 430a1ee95410de818a7a5220e47c5656ed574e35
+import { createCalendar } from "./parser";
+const button = document.getElementById("btn");
+button.onclick = getData;
+function getData() {
     return __awaiter(this, void 0, void 0, function* () {
-        let queryMatch = url.match(/\/(\d+)\&/);
-        let ID = queryMatch ? queryMatch[1] : null;
-        let semCode;
-        try {
-            const res = yield window.fetch(url, {
-                headers: {
-                    "access-control-allow-credentials": "true",
-                    "access-control-allow-headers": "accept,authorization,access-control-allow-headers,access-control-allow-origin",
-                    "access-control-allow-methods": "GET,OPTIONS",
-                    "Authorization": token
-                }
-            });
-            yield res.json().then(resData => {
-                semCode = resData.data.query.rows[0]["A.STRM"];
-            });
-        }
-        catch (error) {
-            console.error(error);
-        }
-        return { ID, semCode };
+        let rawData = {};
+        yield browser.storage.local.get().then(data => rawData = data.data);
+        let calander = createCalendar("test", rawData);
+        console.log("calander");
     });
 }
-function getTimetable(e) {
-    return __awaiter(this, void 0, void 0, function* () {
-        browser.webRequest.onBeforeSendHeaders.removeListener(getTimetable);
-        let token = e.requestHeaders.find((data) => data.name === "Authorization").value;
-        let timetableData;
-        let ID, semCode;
-        yield getIDandCode(e.url, token).then(data => {
-            ID = data.ID;
-            semCode = data.semCode;
-        });
-        try {
-            const res = yield window.fetch(`https://api.adelaide.edu.au/api/generic-query-structured/v1/?target=/system/TIMETABLE_LIST/queryx/${ID},${semCode}&MaxRows=9999`, {
-                headers: {
-                    "access-control-allow-credentials": "true",
-                    "access-control-allow-headers": "accept,authorization,access-control-allow-headers,access-control-allow-origin",
-                    "access-control-allow-methods": "GET",
-                    "Authorization": token
-                }
-            });
-            timetableData = yield res.json();
-        }
-        catch (err) {
-            timetableData = err;
-            console.error(err);
-            return err;
-        }
-<<<<<<< HEAD
-=======
-        browser.webRequest.onBeforeSendHeaders.removeListener(getTimetable);
->>>>>>> 430a1ee95410de818a7a5220e47c5656ed574e35
-        return timetableData;
-    });
-}
-browser.webRequest.onBeforeSendHeaders.addListener(getTimetable, { urls: [targetURL] }, ["requestHeaders"]);
--- src/main.ts
+++ src/main.ts
@@ -1,58 +1,13 @@
-import "@types/firefox-webext-browser";
+import {createCalendar} from "./parser";
 
-const targetURL = "https://api.adelaide.edu.au/api/generic-query-structured/v1/?target=/system/TIMETABLE_WIDGET/queryx/*";
+const button = document.getElementById("btn")!;
+button.onclick = getData;
 
-async function getIDandCode(url : string, token : string) {
-  let queryMatch = url.match(/\/(\d+)\&/);
-  let ID = queryMatch ? queryMatch[1] : null;
-  let semCode;
-  try {
-    const res = await window.fetch(url, {
-      headers: {
-        "access-control-allow-credentials": "true",
-        "access-control-allow-headers": "accept,authorization,access-control-allow-headers,access-control-allow-origin",
-        "access-control-allow-methods": "GET,OPTIONS",
-        "Authorization": token
-      }
-    });
-    await res.json().then(resData => {
-      semCode = resData.data.query.rows[0]["A.STRM"];
-    })
-  } catch (error) {
-    console.error(error);
-  }
-  return {ID, semCode};
+async function getData() {
+  let rawData = {};
+  await browser.storage.local.get().then(data => rawData = data.data);
+
+  let calander = createCalendar("test", rawData);
+
+  console.log("calander");
 }
-
-async function getTimetable(e: any) {
-  browser.webRequest.onBeforeSendHeaders.removeListener(getTimetable);
-  let token = e.requestHeaders.find((data: any) => data.name === "Authorization").value;
-  let timetableData;
-  let ID, semCode;
-  await getIDandCode(e.url, token).then(data => {
-    ID = data.ID;
-    semCode = data.semCode;
-  });
-  try {
-    const res = await window.fetch(`https://api.adelaide.edu.au/api/generic-query-structured/v1/?target=/system/TIMETABLE_LIST/queryx/${ID},${semCode}&MaxRows=9999`, {
-      headers: {
-        "access-control-allow-credentials": "true",
-        "access-control-allow-headers": "accept,authorization,access-control-allow-headers,access-control-allow-origin",
-        "access-control-allow-methods": "GET",
-        "Authorization": token
-      }
-    });
-
-    timetableData = await res.json();
-  } catch (err) {
-    console.error(err);
-    return err;
-  }
-  return timetableData;
-}
-
-browser.webRequest.onBeforeSendHeaders.addListener(
-  getTimetable,
-  { urls: [targetURL] },
-  ["requestHeaders"]
-);
