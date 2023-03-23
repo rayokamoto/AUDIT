198a3c8f441e367b02513e5cbea3babe912ab2a1
Rishi Adhikari <rishiad@users.noreply.github.com>
2023-03-24 00:34:01 +1030

    refactor: Retire webextension polyfill & chrome apis

---
+++ src/firefox/main.ts
@@ -0,0 +1,18 @@
+import { createCalendar, generateICal } from "../parser/parser";
+
+console.info("firefox main.js initialized")
+
+const button = document.getElementById("btn")!;
+button.onclick = getData;
+
+async function getData() {
+  let rawData = {};
+  await browser.storage.local.get().then(data => rawData = data);
+
+  let calander = createCalendar("test", rawData);
+  let ical = generateICal(calander);
+  const downloadLink = document.createElement('a');
+  downloadLink.href = ical;
+  downloadLink.download = "test.ical";
+  downloadLink.click();
+}
