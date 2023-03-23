c9215b0934a7d10f216e3ca94b945a0ca7738e91
Rishi Adhikari <rishiad@users.noreply.github.com>
2023-03-24 00:18:26 +1030

    chore: Build script setup for Chrome and Firefox
    - Setup vite to build using modes flags (chrome and firefox)
    - Setup build.py to run the specific commands for chrome and firefox sequentially

--- build.py
+++ build.py
@@ -2,10 +2,23 @@
 
 import subprocess
 import shutil
+import os
 
-# TODO: Copy across dependencies as well
+subprocess.run("npm run build-chrome", shell=True)
+subprocess.run("npm run build-firefox", shell=True)
+subprocess.run("npx tsc ./src/firefox/background/background.ts --outDir ./extension/firefox/background", shell=True)
 
-subprocess.run("npm run build && npx tsc ./src/background/background.ts --outDir ./extension/background ", shell=True)
-shutil.copyfile("src/manifest.json", "extension/manifest.json")
-shutil.copyfile("src/popup.html", "extension/popup.html")
-shutil.copytree("src/css", "extension/css", dirs_exist_ok=True)
+shutil.copyfile("src/manifests/chrome/manifest.json", "extension/chrome/manifest.json")
+shutil.copyfile("src/manifests/firefox/manifest.json", "extension/firefox/manifest.json")
+
+shutil.copyfile("src/popup.html", "extension/chrome/popup.html")
+shutil.copyfile("src/popup.html", "extension/firefox/popup.html")
+
+shutil.copytree("src/css", "extension/chrome/", dirs_exist_ok=True)
+shutil.copytree("src/css", "extension/firefox/css", dirs_exist_ok=True)
+
+os.mkdir("extension/firefox/icons")
+os.mkdir("extension/chrome/icons")
+shutil.copyfile("src/icon.png", "extension/firefox/icons/icon.png")
+shutil.copyfile("src/icon.png", "extension/chrome/icons/icon.png") 
+
