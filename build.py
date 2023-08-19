2a857795958636dda62770b1b144e7482cd57718
Ray <22254748+rayokamoto@users.noreply.github.com>
2023-08-20 02:38:25 +0930

    chore: Update version from 1.1.0 to 1.2.0

--- build.py
+++ build.py
@@ -6,26 +6,36 @@ import shutil
 import subprocess
 import sys
 
+
 def build_chrome():
     subprocess.run("npm run build-chrome", shell=True)
-    shutil.copyfile("src/manifests/chrome/manifest.json", "extension/chrome/manifest.json")
-    
+    shutil.copyfile(
+        "src/manifests/chrome/manifest.json", "extension/chrome/manifest.json"
+    )
+
     shutil.copyfile("src/popup.html", "extension/chrome/popup.html")
     shutil.copytree("src/css", "extension/chrome/css", dirs_exist_ok=True)
 
     Path("extension/chrome/icons").mkdir(exist_ok=True)
     shutil.copyfile("src/icon.png", "extension/chrome/icons/icon.png")
 
+
 def build_firefox():
     subprocess.run("npm run build-firefox", shell=True)
-    subprocess.run("npx tsc ./src/firefox/background/background.ts --outDir ./extension/firefox/background", shell=True)
+    subprocess.run(
+        "npx tsc ./src/firefox/background/background.ts --outDir ./extension/firefox/background",
+        shell=True,
+    )
 
-    shutil.copyfile("src/manifests/firefox/manifest.json", "extension/firefox/manifest.json")
+    shutil.copyfile(
+        "src/manifests/firefox/manifest.json", "extension/firefox/manifest.json"
+    )
     shutil.copyfile("src/popup.html", "extension/firefox/popup.html")
     shutil.copytree("src/css", "extension/firefox/css", dirs_exist_ok=True)
 
     Path("extension/firefox/icons").mkdir(exist_ok=True)
     shutil.copyfile("src/icon.png", "extension/firefox/icons/icon.png")
+
 
 def update_version(fp, version, msg):
     with open(fp, "r+") as f:
@@ -37,12 +47,15 @@ def update_version(fp, version, msg):
     if msg != "":
         print(msg)
 
+
 def build_prod(version=None):
-    """ Build with extra steps for production """
+    """Build with extra steps for production"""
 
     if version is None:
         try:
-            version = subprocess.check_output("git describe --tags --abbrev=0", shell=True, stderr=subprocess.DEVNULL)
+            version = subprocess.check_output(
+                "git describe --tags --abbrev=0", shell=True, stderr=subprocess.DEVNULL
+            )
             version = str(version)[3:-3]
         except subprocess.CalledProcessError as e:
             version = "1.0.0"
@@ -50,8 +63,12 @@ def build_prod(version=None):
         print(f"Current version: {version}")
 
     update_version("package.json", version, "Updated package.json")
-    update_version("src/manifests/chrome/manifest.json", version, "Updated Chrome manifest.json")
-    update_version("src/manifests/firefox/manifest.json", version, "Updated Firefox manifest.json")
+    update_version(
+        "src/manifests/chrome/manifest.json", version, "Updated Chrome manifest.json"
+    )
+    update_version(
+        "src/manifests/firefox/manifest.json", version, "Updated Firefox manifest.json"
+    )
 
     build_chrome()
     build_firefox()
@@ -67,6 +84,7 @@ def build_prod(version=None):
     shutil.move("./firefox.zip", "extension/")
     shutil.move("./chrome.zip", "extension/")
 
+
 def main(argv):
     if len(argv) == 1:
         build_chrome()
@@ -75,7 +93,7 @@ def main(argv):
         if argv[1] == "prod" and len(argv) == 3:
             build_prod(argv[2])
         elif argv[1] == "prod":
-                build_prod()
+            build_prod()
         elif argv[1] == "chrome":
             build_chrome()
         elif argv[1] == "firefox":
@@ -84,6 +102,7 @@ def main(argv):
             print("Not a valid option")
     else:
         print("Not a valid option")
+
 
 if __name__ == "__main__":
     argv = sys.argv
